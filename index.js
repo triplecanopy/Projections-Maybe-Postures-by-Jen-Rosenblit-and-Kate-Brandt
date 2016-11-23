e91d4b986bbc62426e0c8b3e4c75f13a7aec18c0
Maxwell Simmer <maxwell.simmer@gmail.com>
2016-11-23 20:21:36 +0100

    Get static files

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'))
+app.get('/', function(req, res) {
+  res.render('pages/index')
+})
 
 // views is directory for all template files
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
-
-app.get('/', function(req, res) {
-  res.render('pages/index')
-})
 
 app.get('/api/media', function(req, res) {
   res.setHeader('Content-Type', 'application/json')
@@ -34,6 +32,8 @@ app.get('/api/media', function(req, res) {
     res.send({ body: JSON.stringify(media.filter(Boolean)) })
   })
 })
+
+app.use(express.static(__dirname + '/public'))
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'))
