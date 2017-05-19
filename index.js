41a9be9f1c9e030e81209c7f24893e3df315311c
Maxwell Simmer <maxwell.simmer@gmail.com>
2017-05-19 16:07:46 +0200

    Changes for production deployment

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var fs = require('fs')
 var path = require('path')
 var app = express()
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(req, res) {
